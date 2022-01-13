45ab4fb0fe21313fe0082b2ba59b0a6f08a31452
Glitch (glitch-hello-website) <none>
2022-01-13 21:39:47 +0000

    🚏👁 Checkpoint ./index.html:3112458/3872 ./style.css:3112458/541 ./script.js:3112458/6213

--- script.js
+++ script.js
@@ -1,27 +1,85 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
+// Set up audio context
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioContext = new AudioContext();
+let currentBuffer = null;
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function() {
-    // The JS works in conjunction with the 'dipped' code in style.css
-    btn.classList.toggle("dipped");
-  };
+const audio = document.querySelector("#audio");
+audio.onchange = () => {
+  readFile(audio.files[0]);
+}; 
+const output = document.querySelector("#output");
+const copy = document.querySelector("#copy");
+
+let content = {};
+let contentProxy = new Proxy(content, {
+  set: function (target, key, value) {
+    if (key == "base64") {
+      play(value);
+    }
+    target[key] = value;
+    return true;
+  },
+});
+
+function readFile(file) {
+  const reader = new FileReader();
+  reader.addEventListener("load", (event) => {
+    contentProxy.base64 = event.target.result;
+  });
+
+  reader.addEventListener("progress", (event) => {
+    if (event.loaded && event.total) {
+      const percent = (event.loaded / event.total) * 100;
+      console.log(`Progress: ${Math.round(percent)}`);
+    }
+  });
+  reader.readAsDataURL(file);
 }
 
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
+function _base64ToArrayBuffer(base64) {
+  var binary_string = window.atob(base64);
+  var len = binary_string.length;
+  var bytes = new Uint8Array(len);
+  for (var i = 0; i < len; i++) {
+    bytes[i] = binary_string.charCodeAt(i);
+  }
+  return bytes.buffer;
+}
+
+function getString(arr) {
+  let string = "";
+  let decimalPrecision = 2;
+  for (let ind of Object.keys(arr)) {
+    let sample = arr[ind];
+    string = string.concat(
+      `${ind}: (${parseFloat(sample).toFixed(decimalPrecision)}), `
+    );
+  }
+  return string;
+}
+
+function filterData(audioBuffer) {
+  const rawData = audioBuffer.getChannelData(0); // We only need to work with one channel of data
+  const samples = 70; //rawData.length; // Number of samples we want to have in our final data set
+  const blockSize = Math.floor(rawData.length / samples); // Number of samples in each subdivision
+  var filteredData = [];
+  for (let i = 0; i < samples; i++) {
+    let chunk = rawData.slice(i * blockSize, (i + 1) * blockSize - 1)
+    let sum = chunk.reduce((a, b) => Math.abs(a) + Math.abs(b), 0);
+    filteredData.push(sum/chunk.length);
+  }
+  let max = Math.max(...filteredData);
+  filteredData = filteredData.map((x) => x/max)
+  output.innerHTML = getString(filteredData);
+  return filteredData;
+}
+
+function play(base64) {
+  fetch(base64)
+    .then((response) => response.arrayBuffer())
+    .then((arrayBuffer) => audioContext.decodeAudioData(arrayBuffer))
+    .then((audioBuffer) => filterData(audioBuffer));
+  let audio = new Audio(base64);
+  audio.play();
+}
